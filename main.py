365e08e43062015da11d1c4b9f643ba78ec1e94e
John Adams Jr <john.c.adams32@gmail.com>
2022-01-06 20:56:55 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# On Day 19 of 100 days of code I create an etch and sketch game. This is one of a few projects for this day so feel
+# free to look around at my code and have fun drawing!
+
+# Importing modules needed
+from turtle import Turtle, Screen
+
+etch = Turtle()
+
+
+
+
+screen = Screen()
+screen.exitonclick()
